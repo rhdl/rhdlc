2749b6ce0107e9a7e477f534df7e3d37d3cff977
Sameer Puri <sameer@users.noreply.github.com>
2020-07-31 23:44:27 -0400

    fix & test path segment rename

--- src/ident.rs
+++ src/ident.rs
@@ -3,10 +3,10 @@ use syn::Ident;
 /// TODO: check all idents at the very beginning
 /// https://github.com/rust-lang/rust/blob/5ef299eb9805b4c86b227b718b39084e8bf24454/src/librustc_span/symbol.rs#L1592
 pub fn can_be_raw(ident: &Ident) -> bool {
-    ident != "_" && ident != "" && !is_path_segment_keyword(ident)
+    ident != "r#_" && ident != "r#" && !is_path_segment_keyword(ident)
 }
 
 /// https://github.com/rust-lang/rust/blob/5ef299eb9805b4c86b227b718b39084e8bf24454/src/librustc_span/symbol.rs#L1577
 pub fn is_path_segment_keyword(ident: &Ident) -> bool {
-    ident == "super" || ident == "self" || ident == "Self" || ident == "crate"
+    ident == "r#super" || ident == "r#self" || ident == "r#Self" || ident == "r#crate"
 }
