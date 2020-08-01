fa7d47e3dd241cf919341bd838e0d511d1d7eeb0
Sameer Puri <sameer@users.noreply.github.com>
2020-07-31 23:47:08 -0400

    actually test identifier cases

--- src/main.rs
+++ src/main.rs
@@ -89,4 +89,19 @@ mod test {
             assert_eq!(output, expected);
         }
     }
+
+    #[test]
+    fn compile_fail_identifier() {
+        use pretty_assertions::assert_eq;
+        use std::fs;
+
+        for test in fs::read_dir("./test/compile-fail/identifier").unwrap() {
+            let test = test.unwrap();
+            let input = test.path().join("top.rhdl");
+            dbg!(input.to_string_lossy());
+            let expected = fs::read_to_string(test.path().join("expected.txt")).unwrap();
+            let output = super::entry(crate::resolve::ResolutionSource::File(input));
+            assert_eq!(output, expected);
+        }
+    }
 }
